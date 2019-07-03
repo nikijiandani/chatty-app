8452164c48e1c20bea349ec7fb0421f1b3e2b09c
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-02 23:10:02 -0400

    Add loading state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {loading: true}
+  }
+  componentDidMount(){
+    // After 3 seconds, set `loading` to false in the state.
+    setTimeout(() => {
+      this.setState({loading: false}); // this triggers a re-render!
+    }, 3000)
+  }
   render() {
-    return (
-    <div>
-      <NavBar />
-      <MessageList />
-      <ChatBar />
-    </div>
-    );
+    if(this.state.loading){
+      return (<h1>Fetching data from the API...</h1>)
+    } else {
+      return (
+      <div>
+        <NavBar />
+        <MessageList />
+        <ChatBar />
+      </div>
+      )
+    }
   }
 }
 export default App;
